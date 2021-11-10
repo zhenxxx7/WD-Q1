98c4dbbd0f027fa776b2754f7ad5ee71beaea3db
Misael Jordyy <79817432+zhenxxx7@users.noreply.github.com>
2021-11-11 02:14:20 +0700

    Add files via upload

---
+++ asset/js/main.js
@@ -0,0 +1,48 @@
+ScrollReveal({
+  reset: true,
+  distance: '60px',
+  duration: 2500,
+  delay: 400
+});
+
+ScrollReveal().reveal('.name-title', {delay: 600 })
+ScrollReveal().reveal('.socials', {delay: 750 })
+ScrollReveal().reveal('.user-details', {delay: 450 })
+ScrollReveal().reveal('.user-others', {delay: 250 })
+ScrollReveal().reveal('.aws', {delay: 300 })
+ScrollReveal().reveal('.sk', {delay: 400 })
+ScrollReveal().reveal('.skills', {delay: 550 })
+ScrollReveal().reveal('.hb', {delay: 450 })
+ScrollReveal().reveal('.hobby-content', {delay: 550 })
+ScrollReveal().reveal('.cme', {delay: 350 })
+ScrollReveal().reveal('.container', {delay: 450 })
+ScrollReveal().reveal('.footer-container', {delay: 500 })
+ScrollReveal().reveal('.fa-brands.fa-facebook.fa-2xl', {delay: 750 })
+ScrollReveal().reveal('.fa-brands.fa-instagram.fa-2xl', {delay: 750 })
+ScrollReveal().reveal('.fa-brands.fa-steam.fa-2xl', {delay: 750 })
+ScrollReveal().reveal('.fa-brands.fa-github.fa-2xl', {delay: 750 })
+ScrollReveal().reveal('.cp', {delay: 850 })
+
+
+ScrollReveal({
+  origin: "left",
+  reset: true,
+  distance: '60px',
+  duration: 2500,
+  delay: 400
+});
+
+ScrollReveal().reveal('.user', {delay: 300 })
+ScrollReveal().reveal('.personal-data', {delay: 450 })
+
+
+ScrollReveal({
+  origin: "right",
+  reset: true,
+  distance: '60px',
+  duration: 2500,
+  delay: 400
+});
+
+
+ScrollReveal().reveal('.awards', {delay: 450 })
